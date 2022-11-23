570cddb6b792390da5845f20c1979f1d4419d141
Luís Eduardo Zordan Macagnan <luiseduardozordanmacagnan@gmail.com>
2022-11-23 11:15:43 -0300

    Colocado Inserts

---
+++ Script_Inserts.sql
@@ -0,0 +1,98 @@
+insert into foguetes (estado, combustivel, bagagem, destino) values ('Pronto', 70, 15, 'Mercúrio');
+insert into foguetes (estado, combustivel, bagagem, destino) values ('Danificado', 63, 30, 'Netuno');
+insert into foguetes (estado, combustivel, bagagem, destino) values ('Danificado', 71, 31, 'Mercúrio');
+insert into foguetes (estado, combustivel, bagagem, destino) values ('Em construção', 100, 77, 'Vênus');
+insert into foguetes (estado, combustivel, bagagem, destino) values ('Pronto', 64, 24, 'Urano');
+insert into foguetes (estado, combustivel, bagagem, destino) values ('Danificado', 57, 60, 'Marte');
+insert into foguetes (estado, combustivel, bagagem, destino) values ('Danificado', 12, 83, 'Urano');
+insert into foguetes (estado, combustivel, bagagem, destino) values ('Danificado', 24, 97, 'Júpiter');
+insert into foguetes (estado, combustivel, bagagem, destino) values ('Danificado', 34, 8, 'Mercúrio');
+insert into foguetes (estado, combustivel, bagagem, destino) values ('Pronto', 42, 16, 'Vênus');
+
+insert into estacao (operacao) values ('Pesquisa');
+insert into estacao (operacao) values ('Manutenção');
+insert into estacao (operacao) values ('Lançamento');
+insert into estacao (operacao) values ('Pesquisa');
+insert into estacao (operacao) values ('Construção');
+insert into estacao (operacao) values ('Lançamento');
+insert into estacao (operacao) values ('Construção');
+insert into estacao (operacao) values ('Lançamento');
+insert into estacao (operacao) values ('Manutenção');
+insert into estacao (operacao) values ('Construção');
+
+insert into laboratorio (qnt_funcionarios, objetivo) values (2, 'Combústivel mais barato');
+insert into laboratorio (qnt_funcionarios, objetivo) values (14, 'Melhoramento em turbinas');
+insert into laboratorio (qnt_funcionarios, objetivo) values (4, 'Tecnologia capaz de aguentar mais calor');
+insert into laboratorio (qnt_funcionarios, objetivo) values (13, 'Tecnologia capaz de aguentar mais frio');
+insert into laboratorio (qnt_funcionarios, objetivo) values (15, 'Tecnologia capaz de aguentar mais pressão');
+insert into laboratorio (qnt_funcionarios, objetivo) values (2, 'IA para missões');
+insert into laboratorio (qnt_funcionarios, objetivo) values (3, 'Formas de colonizar a lua');
+insert into laboratorio (qnt_funcionarios, objetivo) values (8, 'Melhoramento de telescópio');
+insert into laboratorio (qnt_funcionarios, objetivo) values (14, 'Aumentar segurança lançamentos foguete');
+insert into laboratorio (qnt_funcionarios, objetivo) values (13, 'Nomeação de estrelas');
+
+insert into funcionarios (cpf, telefone, nome_completo, data_nascimento) values ('061983850-7', '+86 (113) 319-4932', 'Kai Hayworth', 9-30-1982);
+insert into funcionarios (cpf, telefone, nome_completo, data_nascimento) values ('220206403-6', '+1 (901) 312-1076', 'Maure Slateford', 8-16-2000);
+insert into funcionarios (cpf, telefone, nome_completo, data_nascimento) values ('637843577-4', '+52 (728) 129-6171', 'Cesya Swanger', 10-20-1984);
+insert into funcionarios (cpf, telefone, nome_completo, data_nascimento) values ('681149328-3', '+375 (509) 989-9690', 'Jessi Swanborrow', 3-9-1993);
+insert into funcionarios (cpf, telefone, nome_completo, data_nascimento) values ('969286182-1', '+48 (481) 384-9448', 'Ninette MacDermand', 5-24-1986);
+insert into funcionarios (cpf, telefone, nome_completo, data_nascimento) values ('698195310-2', '+352 (455) 684-9574', 'Purcell Donnachie', 10-27-1984);
+insert into funcionarios (cpf, telefone, nome_completo, data_nascimento) values ('702099020-7', '+63 (548) 152-0187', 'Cindie Maxstead', 3-29-1981);
+insert into funcionarios (cpf, telefone, nome_completo, data_nascimento) values ('431178767-7', '+62 (412) 928-6107', 'Boycey Gaw', 11-12-1990);
+insert into funcionarios (cpf, telefone, nome_completo, data_nascimento) values ('814769167-3', '+86 (649) 729-2024', 'Robinett Bulmer', 7-28-2000);
+insert into funcionarios (cpf, telefone, nome_completo, data_nascimento) values ('560206716-7', '+386 (614) 116-8723', 'Thacher Lerhinan', 2-16-1999);
+
+insert into cargo (funcao, salario) values ('Cientista', 6859.7);
+insert into cargo (funcao, salario) values ('Pesquisador', 6877.7);
+insert into cargo (funcao, salario) values ('Engenheiro', 4157.9);
+insert into cargo (funcao, salario) values ('Mecânico', 8599.1);
+insert into cargo (funcao, salario) values ('Astronauta', 9559.0);
+insert into cargo (funcao, salario) values ('Segurança', 9467.2);
+insert into cargo (funcao, salario) values ('Zelador', 6163.8);
+insert into cargo (funcao, salario) values ('Programador', 3831.7);
+insert into cargo (funcao, salario) values ('Alienigena de estimação', 6077.6);
+insert into cargo (funcao, salario) values ('Estagiário', 6733.4);
+
+insert into armazem (espaco) values (18);
+insert into armazem (espaco) values (16);
+insert into armazem (espaco) values (53);
+insert into armazem (espaco) values (38);
+insert into armazem (espaco) values (58);
+insert into armazem (espaco) values (68);
+insert into armazem (espaco) values (7);
+insert into armazem (espaco) values (13);
+insert into armazem (espaco) values (46);
+insert into armazem (espaco) values (83);
+
+insert into equipamentos (quantidade, modelo) values (29, 'Telescópios');
+insert into equipamentos (quantidade, modelo) values (4, 'Hubble');
+insert into equipamentos (quantidade, modelo) values (1, 'XMM');
+insert into equipamentos (quantidade, modelo) values (27, 'Chandra');
+insert into equipamentos (quantidade, modelo) values (9, 'Spitzer');
+insert into equipamentos (quantidade, modelo) values (1, 'SOHO');
+insert into equipamentos (quantidade, modelo) values (5, 'ISO');
+insert into equipamentos (quantidade, modelo) values (26, 'Herschel');
+insert into equipamentos (quantidade, modelo) values (7, 'ISO-09');
+insert into equipamentos (quantidade, modelo) values (13, 'Mackmander');
+
+insert into amostras (quantidade, qntAmostra) values (-2, 30);
+insert into amostras (quantidade, qntAmostra) values (3, 20);
+insert into amostras (quantidade, qntAmostra) values (1, 13);
+insert into amostras (quantidade, qntAmostra) values (-2, 20);
+insert into amostras (quantidade, qntAmostra) values (2, 38);
+insert into amostras (quantidade, qntAmostra) values (3, 6);
+insert into amostras (quantidade, qntAmostra) values (-5, 15);
+insert into amostras (quantidade, qntAmostra) values (-4, 2);
+insert into amostras (quantidade, qntAmostra) values (1, 22);
+insert into amostras (quantidade, qntAmostra) values (7, 1);
+
+insert into origem (origem) values ('Mercúrio');
+insert into origem (origem) values ('Vênus');
+insert into origem (origem) values ('Terra');
+insert into origem (origem) values ('Marte');
+insert into origem (origem) values ('Júpiter');
+insert into origem (origem) values ('Saturno');
+insert into origem (origem) values ('Urano');
+insert into origem (origem) values ('Netuno');
+insert into origem (origem) values ('Plutão');
+insert into origem (origem) values ('Pallas');
--- Script_estacao_foguete.sql
+++ Script_estacao_foguete.sql
@@ -1,65 +1,125 @@
+DROP DATABASE estacao_foguete;
 CREATE DATABASE estacao_foguete;
-
 USE estacao_foguete;
 
-CREATE TABLE estacao(
-	id_estacao INT NOT NULL PRIMARY KEY auto_increment,
-    operacao VARCHAR(45) NOT NULL
-);
-
 CREATE TABLE foguetes (
-	id_foguete INT NOT NULL PRIMARY KEY auto_increment,
-    estado VARCHAR(45) NOT NULL,
-    combustivel INT NOT NULL,
-    bagagem INT NOT NULL,
-    destino VARCHAR(45) NOT NULL,
-    FOREIGN KEY (id_estacao) REFERENCES estacao(id_estacao)
-);
+  id_foguete INT NOT NULL AUTO_INCREMENT,
+  estado VARCHAR(45) BINARY NOT NULL,
+  combustivel INT NOT NULL,
+  bagagem INT NOT NULL,
+  destino VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_foguete`));
 
-CREATE TABLE laboratorio(
-	id_laboratorio INT NOT NULL PRIMARY KEY auto_increment,
-    qnt_funcionarios INT NOT NULL,
-    objetivo VARCHAR(45) NOT NULL,
-    FOREIGN KEY (id_estacao) REFERENCES estacao(id_estacao)
-);
+CREATE TABLE cargo (
+  id_cargo INT NOT NULL AUTO_INCREMENT,
+  funcao VARCHAR(45) NOT NULL,
+  salario DOUBLE NOT NULL,
+  PRIMARY KEY (id_cargo));
 
-CREATE TABLE funcionarios(
-	cpf INT NOT NULL PRIMARY KEY,
-    telefone VARCHAR(45) NOT NULL,
-    nome_completo VARCHAR(45) NOT NULL,
-    data_nascimento DATE NOT NULL,
-    FOREIGN KEY (id_laboratorio) REFERENCES laboratorio(id_laboratorio),
-    FOREIGN KEY (id_cargo) REFERENCES cargo(id_cargo)
-);
+CREATE TABLE funcionarios (
+  cpf VARCHAR(45) NOT NULL,
+  telefone VARCHAR(45) NOT NULL,
+  nome_completo VARCHAR(45) NOT NULL,
+  cargo_id_cargo INT NOT NULL,
+  data_nascimento DATE NOT NULL,
+  PRIMARY KEY (cpf, cargo_id_cargo), 
+    FOREIGN KEY (cargo_id_cargo)
+    REFERENCES cargo (id_cargo)
+    );
 
-CREATE TABLE cargo(
-	id_cargo INT NOT NULL PRIMARY KEY,
-    funcao VARCHAR(45) NOT NULL,
-    salario DOUBLE NOT NULL
-);
+CREATE TABLE laboratorio (
+  id_laboratorio INT NOT NULL AUTO_INCREMENT,
+  qnt_funcionarios INT NOT NULL,
+  objetivo VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id_laboratorio));
 
-CREATE TABLE armazem(
-	id_armazem INT NOT NULL PRIMARY KEY auto_increment,
-    espaco INT NOT NULL,
-    FOREIGN KEY (id_estacao) REFERENCES estacao(id_estacao)
-);
+CREATE TABLE equipamentos (
+  id_equipamento INT NOT NULL AUTO_INCREMENT,
+  quantidade INT NOT NULL,
+  modelo VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id_equipamento));
 
-CREATE TABLE equipamentos(
-	id_equipamento INT NOT NULL PRIMARY KEY auto_increment,
-    quantidade INT NOT NULL,
-    modelo VARCHAR(45) NOT NULL,
-    FOREIGN KEY (id_armazem) REFERENCES armazem(id_armazem)
-);
+CREATE TABLE estacao (
+  id_estacao INT NOT NULL AUTO_INCREMENT,
+  operacao VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id_estacao));
 
-CREATE TABLE amostras(
-	id_amostra INT NOT NULL PRIMARY KEY auto_increment,
-    ph INT NOT NULL,
-    qnt_amostra INT NOT NULL,
-    FOREIGN KEY (id_armazem) REFERENCES armazem(id_armazem)
-);
+CREATE TABLE armazem (
+  id_armazem INT NOT NULL AUTO_INCREMENT,
+  espaco INT NOT NULL,
+  PRIMARY KEY (id_armazem));
 
-CREATE TABLE origem(
-	id_origem INT NOT NULL PRIMARY KEY auto_increment,
-    planeta VARCHAR(45) NOT NULL,
-    FOREIGN KEY (id_amostra) REFERENCES amostras(id_amostra)
-);
+CREATE TABLE amostras (
+  id_amostra INT NOT NULL AUTO_INCREMENT,
+  ph INT NOT NULL,
+	qntAmostra INT NOT NULL,
+  PRIMARY KEY (id_amostra));
+
+CREATE TABLE origem (
+  id_origem INT NOT NULL AUTO_INCREMENT,
+  planeta VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id_origem));
+
+CREATE TABLE foguetes_has_estacao (
+  foguetes_id_foguete INT NOT NULL,
+  estacao_id_estacao INT NOT NULL,
+  PRIMARY KEY (foguetes_id_foguete, estacao_id_estacao),
+    FOREIGN KEY (foguetes_id_foguete)
+    REFERENCES foguetes (id_foguete),
+    FOREIGN KEY (estacao_id_estacao)
+    REFERENCES estacao (id_estacao));
+
+CREATE TABLE amostras_has_Origem (
+  amostras_id_amostra INT NOT NULL,
+  origem_id_origem INT NOT NULL,
+  PRIMARY KEY (amostras_id_amostra, origem_id_origem),
+    FOREIGN KEY (amostras_id_amostra)
+    REFERENCES amostras (id_amostra),
+    FOREIGN KEY (origem_id_origem)
+    REFERENCES origem (id_origem));
+
+CREATE TABLE armazem_has_equipamentos (
+  armazem_id_armazem INT NOT NULL,
+  equipamentos_id_equipamento INT NOT NULL,
+  PRIMARY KEY (armazem_id_armazem, equipamentos_id_equipamento),
+    FOREIGN KEY (armazem_id_armazem)
+    REFERENCES Armazem (id_armazem),
+    FOREIGN KEY (equipamentos_id_equipamento)
+    REFERENCES equipamentos (Id_equipamento));
+
+CREATE TABLE laboratorio_has_funcionarios (
+  laboratorio_id_laboratorio INT NOT NULL,
+  funcionarios_cpf VARCHAR(45) NOT NULL,
+  PRIMARY KEY (laboratorio_id_laboratorio, funcionarios_cpf),
+    FOREIGN KEY (laboratorio_id_laboratorio)
+    REFERENCES laboratorio (id_laboratorio),
+    FOREIGN KEY (funcionarios_cpf)
+    REFERENCES funcionarios (cpf));
+
+CREATE TABLE armazem_has_amostras (
+  armazem_id_armazem INT NOT NULL,
+  amostras_id_amostras INT NOT NULL,
+  PRIMARY KEY (armazem_id_armazem, amostras_id_amostras),
+    FOREIGN KEY (armazem_id_armazem)
+    REFERENCES armazem (id_armazem),
+    FOREIGN KEY (amostras_id_amostras)
+    REFERENCES amostras (id_amostra));
+
+CREATE TABLE estacao_has_laboratorio (
+  estacao_id_estacao INT NOT NULL,
+  laboratorio_id_laboratorio INT NOT NULL,
+  PRIMARY KEY (estacao_id_estacao, laboratorio_id_laboratorio),
+    FOREIGN KEY (estacao_id_estacao)
+    REFERENCES estacao (id_estacao),
+    FOREIGN KEY (laboratorio_id_laboratorio)
+    REFERENCES laboratorio (id_laboratorio));
+
+CREATE TABLE estacao_has_armazem (
+  estacao_id_estacao INT NOT NULL,
+  armazem_id_armazem INT NOT NULL,
+  PRIMARY KEY (estacao_id_estacao, armazem_id_armazem),
+    FOREIGN KEY (estacao_id_estacao)
+    REFERENCES estacao (id_estacao),
+    FOREIGN KEY (armazem_id_armazem)
+    REFERENCES armazem (id_armazem));
+
